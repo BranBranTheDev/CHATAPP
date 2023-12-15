2e0ae5f8598c27da084dfc0a46b0ef494b2780c4
Brandon <bran9bran6@gmail.com>
2023-12-15 12:15:48 -0700

    Added messaging logic to UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from './components/ChatFeed'; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <ChatEngine
+            height='100vh'
+            projectID="6f6ab379-76d5-48c9-9479-a196d5dd78fd"
+            userName="thatOneGuysAccount"
+            userSecret="123123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+        />
+    );
 }
 
 export default App;
+
+//https://www.youtube.com/watch?v=jcOKU9f86XE&t=381s
+//left off at 12:46
